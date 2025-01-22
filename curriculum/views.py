133e91dfef32b2a34b5d9a017e5d529795c1d844
Samuthe <ekatisamuel@gmail.com>
2025-01-22 23:07:22 +0300

    initial app

---
+++ curriculum/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+# from django.http import HttpResponse
+
+def home(request):
+    return render(request, 'base.html')
+
+def about(request):
+    return render(request, 'curriculum/about.html')
